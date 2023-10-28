590f4fb4333ec2d4309c0694d58a2bd5536781cb
Natalya2121 <andriyenkonatalya@gmail.com>
2023-10-28 19:52:16 +0300

    js-auth2

--- src/class/user.js
+++ src/class/user.js
@@ -23,8 +23,17 @@ class User {
   }
 
   static create(data) {
-    user = new User(data)
+    const user = new User(data)
+    console.log(user)
     this.#list.push(user)
+    console.log(this.#list)
+  }
+
+  static getByEmail(email) {
+    return (
+      this.#list.find((user) => user.email === email) ||
+      null
+    )
   }
 }
 
